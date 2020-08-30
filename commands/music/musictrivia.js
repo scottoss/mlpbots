0d61a4f1b4372104026b2485ebbf4185c293fb3a
derpyhooves <56843137+scottoss@users.noreply.github.com>
2020-08-30 11:30:50 +0200

    Update musictrivia.js

--- commands/music/musictrivia.js
+++ commands/music/musictrivia.js
@@ -24,7 +24,7 @@ module.exports = class MusicTriviaCommand extends Command {
           prompt: 'What is the number of songs you want the quiz to have?',
           type: 'integer',
           default: 6,
-          max: 20
+          max: 10
         }
       ]
     });
